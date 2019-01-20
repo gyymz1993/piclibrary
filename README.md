89e28ea23b83ddb85610cca1a1bdf43efa82e47a
gyymz1993 <541993081@qq.com>
2019-01-20 09:57:46 +0800

    更新库

---
+++ README.md
@@ -0,0 +1,6 @@
+# piclibrary 图片选择器
+必须依赖库
+    /*Rx引入包*/
+    api 'io.reactivex.rxjava2:rxandroid:2.1.0'
+    /*RxAndroid*/
+    api 'io.reactivex.rxjava2:rxjava:2.2.0'
--- app/src/main/java/com/example/administrator/mylibrary/MainActivity.java
+++ app/src/main/java/com/example/administrator/mylibrary/MainActivity.java
@@ -1,7 +1,18 @@
 package com.example.administrator.mylibrary;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.luck.picture.lib.PictureSelector;
+import com.luck.picture.lib.config.PictureConfig;
+import com.luck.picture.lib.config.PictureMimeType;
+import com.luck.picture.lib.entity.LocalMedia;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +20,74 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.id_to_go).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"gotoSelectPhotoType",Toast.LENGTH_LONG).show();
+                gotoSelectPhotoType();
+            }
+        });
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case PictureConfig.CHOOSE_REQUEST:
+                    // 图片选择结果回调
+                    List<LocalMedia> localMedia = PictureSelector.obtainMultipleResult(data);
+                    // 例如 LocalMedia 里面返回三种path
+                    // 1.media.getPath(); 为原图path
+                    // 2.media.getCutPath();为裁剪后path，需判断media.isCut();是否为true
+                    // 3.media.getCompressPath();为压缩后path，需判断media.isCompressed();是否为true
+                    // 如果裁剪并压缩了，已取压缩路径为准，因为是先裁剪后压缩的
+                    String imageUrl = null;
+                    for (LocalMedia media : localMedia) {
+                        Log.i("图片-----》", media.getPath());
+                        imageUrl = media.getPath();
+                    }
+                    // Drawable fromPath = Drawable.createFromPath(imageUrl);
+                    //idRgUseravatar.setBackgroundDrawable(fromPath);
+                   // ImageLoader.getInstance().showImage(getContext(), imageUrl, idRgUseravatar);
+                    break;
+            }
+        }
+    }
+
+    public void onClickSelectPhotoType(View view) {
+        gotoSelectPhotoType();
+    }
+
+
+
+    private void gotoSelectPhotoType() {
+        int chooseMode = PictureMimeType.ofImage();
+        // 进入相册 以下是例子：不需要的api可以不写
+        PictureSelector.create(MainActivity.this)
+                .openGallery(chooseMode)// 全部.PictureMimeType.ofAll()、图片.ofImage()、视频.ofVideo()、音频.ofAudio()
+                .theme(R.style.picture_default_style)// 主题样式设置 具体参考 values/styles   用法：R.style.picture.white.style
+                .maxSelectNum(1)// 最大图片选择数量
+                .minSelectNum(1)// 最小选择数量
+                .imageSpanCount(4)// 每行显示个数
+                .selectionMode(PictureConfig.SINGLE)// 多选 or 单选
+                .isCamera(true)// 是否显示拍照按钮
+                .isZoomAnim(true)// 图片列表点击 缩放效果 默认true
+                .enableCrop(true)// 是否裁剪
+                .compress(true)// 是否压缩
+                .synOrAsy(true)//同步true或异步false 压缩 默认同步
+                .glideOverride(160, 160)// glide 加载宽高，越小图片列表越流畅，但会影响列表图片浏览的清晰度
+                .hideBottomControls(false)// 是否显示uCrop工具栏，默认不显示
+                .isGif(false)// 是否显示gif图片
+                .freeStyleCropEnabled(true)// 裁剪框是否可拖拽
+                .circleDimmedLayer(false)// 是否圆形裁剪
+                .showCropFrame(true)// 是否显示裁剪矩形边框 圆形裁剪时建议设为false
+                .showCropGrid(false)// 是否显示裁剪矩形网格 圆形裁剪时建议设为false
+                .openClickSound(false)// 是否开启点击声音
+                //.selectionMedia(false)// 是否传入已选图片
+                .minimumCompressSize(100)// 小于100kb的图片不压缩
+                .forResult(PictureConfig.CHOOSE_REQUEST);//结果回调onActivityResult code
+    }
+
+
 }
